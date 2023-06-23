a62620d2bf49f33da0132854eb332d9ec2b8594e
ArmSiwatK <arm.siwatk@gmail.com>
2023-06-23 11:32:30 +0700

    style: Simple background

---
+++ src/components/GridBackground/GridBackground.tsx
@@ -0,0 +1,20 @@
+/*
+<--------------- Imports and Interface --------------->
+*/
+
+import React from "react";
+import "./GridBackground.scss";
+
+/*
+<--------------- Component --------------->
+*/
+
+const GridBackground: React.FC = () => {
+  /*
+  <--------------- Rendering --------------->
+  */
+
+  return <div className="grid"></div>;
+};
+
+export default GridBackground;
