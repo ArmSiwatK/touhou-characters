37b07e4e352621ac2d0b5a8f55a230119fd6c6b4
ArmSiwatK <arm.siwatk@gmail.com>
2023-06-23 19:29:51 +0700

    fix: Title positioning

--- src/components/PortraitDisplay/PortraitDisplay.tsx
+++ src/components/PortraitDisplay/PortraitDisplay.tsx
@@ -116,34 +116,31 @@ const PortraitDisplay: React.FC<PortraitDisplayProps> = ({
         <div className="character-info">
           <h2 className="character-name">{selectedCharacter.name}</h2>
           <div className="portrait-gallery">
-            <>
-              {!isMobile &&
-                [previousCharacter, selectedCharacter, nextCharacter].map(
-                  (character) => (
-                    <div
-                      className={`portrait-item ${
+            {!isMobile &&
+              [previousCharacter, selectedCharacter, nextCharacter].map(
+                (character) => (
+                  <div
+                    className={`portrait-item ${
+                      character.charId === selectedCharacter.charId
+                        ? "selected"
+                        : "side"
+                    }`}
+                    key={character.charId}
+                    onClick={() => setSelectedCharacter(character)}
+                  >
+                    <img
+                      src={
                         character.charId === selectedCharacter.charId
-                          ? "selected"
-                          : "side"
-                      }`}
-                      key={character.charId}
-                      onClick={() => setSelectedCharacter(character)}
-                    >
-                      <img
-                        src={
-                          character.charId === selectedCharacter.charId
-                            ? `/portraits/${selectedEmotion.toLowerCase()}/${
-                                character.charId
-                              }.png`
-                            : `/portraits/neutral/${character.charId}.png`
-                        }
-                        alt={`Character portrait – ${character.name}`}
-                      />
-                    </div>
-                  )
-                )}
-              <h2 className="character-title">{selectedCharacter.title}</h2>
-            </>
+                          ? `/portraits/${selectedEmotion.toLowerCase()}/${
+                              character.charId
+                            }.png`
+                          : `/portraits/neutral/${character.charId}.png`
+                      }
+                      alt={`Character portrait – ${character.name}`}
+                    />
+                  </div>
+                )
+              )}
             {isMobile && (
               <>
                 <div className="portrait-item selected">
@@ -162,6 +159,9 @@ const PortraitDisplay: React.FC<PortraitDisplayProps> = ({
               </>
             )}
           </div>
+          {!isMobile && (
+            <h2 className="character-title">{selectedCharacter.title}</h2>
+          )}
         </div>
       )}
     </div>
