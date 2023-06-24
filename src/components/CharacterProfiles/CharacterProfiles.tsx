0661f3a1cf566ec3fa293c57e15580cc5ebda392
ArmSiwatK <arm.siwatk@gmail.com>
2023-06-24 13:29:56 +0700

    refactor: Move responsive to TSX

--- src/components/CharacterProfiles/CharacterProfiles.tsx
+++ src/components/CharacterProfiles/CharacterProfiles.tsx
@@ -2,7 +2,7 @@
 <--------------- Imports and Interface --------------->
 */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Character } from "../../utilities";
 import characters from "../../assets/characters.json";
 import "./CharacterProfiles.scss";
@@ -23,28 +23,77 @@ const CharacterProfiles: React.FC<CharacterProfilesProps> = ({
   setSelectedCharacter,
 }) => {
   /*
-  <--------------- Functions --------------->
+  <--------------- States and Variables --------------->
   */
-  const getFilteredCharacters = () => {
-    return selectedCategory === "All"
+
+  const [columnCount, setColumnCount] = useState(9);
+  const [shouldRender, setShouldRender] = useState(true);
+
+  const filteredCharacters =
+    selectedCategory === "All"
       ? characters
       : characters.filter(
           (character) => character.category === selectedCategory
         );
-  };
+  const gridTemplateColumns = `repeat(${columnCount}, 1fr)`;
+
+  /*
+  <--------------- Functions --------------->
+  */
 
   const handleCharacterClick = (character: Character) => {
     setSelectedCharacter(character);
   };
 
+  const handleResize = () => {
+    const screenWidth = window.innerWidth;
+
+    if (filteredCharacters.length > columnCount) {
+      if (screenWidth >= 1575) {
+        setColumnCount(9);
+      } else if (screenWidth >= 1425) {
+        setColumnCount(8);
+      } else if (screenWidth >= 1250) {
+        setColumnCount(7);
+      } else if (screenWidth >= 1100) {
+        setColumnCount(6);
+      } else if (screenWidth >= 940) {
+        setColumnCount(5);
+      } else {
+        setColumnCount(4);
+      }
+    } else {
+      setColumnCount(filteredCharacters.length);
+    }
+
+    setShouldRender(screenWidth >= 768);
+  };
+
+  /*
+  <--------------- useEffect Hook --------------->
+  */
+
+  useEffect(() => {
+    handleResize();
+  }, [columnCount, filteredCharacters.length]);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   /*
   <--------------- Rendering --------------->
   */
 
-  const filteredCharacters = getFilteredCharacters();
-
-  return (
-    <div className="character-profiles">
+  return shouldRender ? (
+    <div
+      className="character-profiles"
+      style={{ gridTemplateColumns: gridTemplateColumns }}
+    >
       {filteredCharacters.map((character) => (
         <div
           className={`character-profile ${
@@ -60,7 +109,7 @@ const CharacterProfiles: React.FC<CharacterProfilesProps> = ({
         </div>
       ))}
     </div>
-  );
+  ) : null;
 };
 
 export default CharacterProfiles;
