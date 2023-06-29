3867f9fd14828b32cafa8d2f95578f33f801d998
ArmSiwatK <arm.siwatk@gmail.com>
2023-06-29 12:31:12 +0700

    style: Use simple gradient background

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import GridBackground from "./components/GridBackground/GridBackground";
 import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-    <GridBackground />
   </React.StrictMode>
 );
